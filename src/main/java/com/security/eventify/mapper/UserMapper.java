b6ed5b435cd48bbcd1ecdae653b632fc3ba240ab
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-18 08:22:00 +0100

    Mapper : ajouter user mapper

--- src/main/java/com/security/eventify/mapper/UserMapper.java
+++ src/main/java/com/security/eventify/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package com.security.eventify.mapper;
 
-public class UserMapper {
+import com.security.eventify.dto.userDto.UserDto;
+import com.security.eventify.dto.userDto.UserRegisterDto;
+import com.security.eventify.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDto userToUserDto(User user);  // pour retourner un utilisateur sans mot de pass
+    @Mapping(target = "password", ignore = true) // ignorer le mot de pass pour saisir au service
+    @Mapping(target="id" , ignore = true)
+    User userRegisterDtoToUser(UserRegisterDto userDto);
 }
