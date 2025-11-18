efa10667992fb6f2848da0c097a319576ebb644d
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-18 08:10:00 +0100

    Dto : ajouter 2 dto pour l'enregistrement et pour l afiichage

--- src/main/java/com/security/eventify/dto/userDto/UserDto.java
+++ src/main/java/com/security/eventify/dto/userDto/UserDto.java
@@ -1,8 +1,9 @@
-package com.security.eventify.dto;
+package com.security.eventify.dto.userDto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+// pour afficher les donnes d'utilisateur
 @Data
 public class UserDto {
     private int id;
