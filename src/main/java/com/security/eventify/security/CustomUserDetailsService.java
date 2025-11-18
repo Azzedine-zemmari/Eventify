0752516291c2de0dc4044329bc52bffd35b4c0e8
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-18 17:55:47 +0100

    Security : add custom user details service

---
+++ src/main/java/com/security/eventify/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.security.eventify.security;
+
+import com.security.eventify.model.User;
+import com.security.eventify.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException(username));
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.singletonList(authority))
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
