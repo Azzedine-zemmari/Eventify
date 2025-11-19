18adfea41fd661129fbdd88a5be3a8b79e07742b
asma828 <asmalachhab66@gmail.com>
2025-11-19 09:32:41 +0100

    create event repository

--- src/main/java/com/security/eventify/repository/EventRepository.java
+++ src/main/java/com/security/eventify/repository/EventRepository.java
@@ -1,4 +1,9 @@
 package com.security.eventify.repository;
 
-public class EventRepository {
+import com.security.eventify.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+    List<Event> findByOrganizerId(int organizerId);
 }
