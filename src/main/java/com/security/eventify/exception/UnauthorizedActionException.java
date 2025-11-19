92c5d6490e3be38dac0c9a74054b3ede12bf75c7
asma828 <asmalachhab66@gmail.com>
2025-11-19 15:28:48 +0100

    create an exception for unauthorizeaction

--- src/main/java/com/security/eventify/exception/UnauthorizedActionException.java
+++ src/main/java/com/security/eventify/exception/UnauthorizedActionException.java
@@ -1,4 +1,7 @@
 package com.security.eventify.exception;
 
-public class UnauthorizedActionException {
+public class UnauthorizedActionException extends RuntimeException {
+    public UnauthorizedActionException(String message) {
+        super(message);
+    }
 }
