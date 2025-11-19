5bee2bb0168a6774097cf69602fc8609e1577186
asma828 <asmalachhab66@gmail.com>
2025-11-19 09:29:43 +0100

    create registration entity

--- src/main/java/com/security/eventify/model/Registration.java
+++ src/main/java/com/security/eventify/model/Registration.java
@@ -1,4 +1,28 @@
 package com.security.eventify.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int userId;
+    private int eventId;
+    private LocalDateTime registeredAt;
+
+    @Enumerated(EnumType.STRING)
+    private RegistrationStatus status;
+
+    @PrePersist
+    protected void onCreate() {
+        registeredAt = LocalDateTime.now();
+        if (status == null) {
+            status = RegistrationStatus.CONFIRMED;
+        }
+    }
 }
