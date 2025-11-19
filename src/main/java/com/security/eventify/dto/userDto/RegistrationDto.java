8963b3fffeeed30ef260b8fe3d916574251592bc
asma828 <asmalachhab66@gmail.com>
2025-11-19 09:31:59 +0100

    create registration dto

--- src/main/java/com/security/eventify/dto/userDto/RegistrationDto.java
+++ src/main/java/com/security/eventify/dto/userDto/RegistrationDto.java
@@ -1,4 +1,14 @@
 package com.security.eventify.dto.userDto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class RegistrationDto {
+    private int id;
+    private int userId;
+    private int eventId;
+    private String eventTitle;
+    private LocalDateTime registeredAt;
+    private String status;
 }
