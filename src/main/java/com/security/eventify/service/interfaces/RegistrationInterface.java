4a04e937bdf4730ccc5f861a4526fc2a7a65e85e
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-20 11:10:00 +0100

    Feature: ajouter Registration interface service

--- src/main/java/com/security/eventify/service/interfaces/RegistrationInterface.java
+++ src/main/java/com/security/eventify/service/interfaces/RegistrationInterface.java
@@ -1,4 +1,7 @@
 package com.security.eventify.service.interfaces;
 
+import com.security.eventify.dto.userDto.RegistrationDto;
+
 public interface RegistrationInterface {
+    RegistrationDto registrer(int eventId , String email);
 }
