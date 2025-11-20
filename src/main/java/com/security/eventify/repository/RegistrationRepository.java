3591cc726ddb846bf0a0e682cfea70607c5c8899
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-20 15:20:16 +0100

    Feature : ajouter method find all by user id in repository

--- src/main/java/com/security/eventify/repository/RegistrationRepository.java
+++ src/main/java/com/security/eventify/repository/RegistrationRepository.java
@@ -3,5 +3,8 @@ package com.security.eventify.repository;
 import com.security.eventify.model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegistrationRepository extends JpaRepository<Registration , Integer> {
+    List<Registration> findAllByUser_Id(int userId);
 }
