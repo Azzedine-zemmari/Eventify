454b2f6c44979a42934a11981865b312239d141e
asma828 <asmalachhab66@gmail.com>
2025-11-21 15:56:11 +0100

    add restAuthenticationEntryPoint and customAccessDeniedHandler erreur handling to the config security file

--- src/main/java/com/security/eventify/security/SecurityConfig.java
+++ src/main/java/com/security/eventify/security/SecurityConfig.java
@@ -2,32 +2,38 @@ package com.security.eventify.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     private final CustomAuthenticationProvider customAuthenticationProvider;
-    public SecurityConfig(CustomAuthenticationProvider customAuthenticationProvider){
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    public SecurityConfig(CustomAuthenticationProvider customAuthenticationProvider,
+                          RestAuthenticationEntryPoint restAuthenticationEntryPoint,
+                          CustomAccessDeniedHandler customAccessDeniedHandler) {
         this.customAuthenticationProvider = customAuthenticationProvider;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.customAccessDeniedHandler = customAccessDeniedHandler;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf->csrf.disable()) // pour disable le csrf
-                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(customAuthenticationProvider)
-                .exceptionHandling(ex->ex
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .authorizeHttpRequests(auth -> auth // pour commenecer les regle du securite sur les route
-                .requestMatchers("/api/v1/users").permitAll()
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(restAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/users").permitAll()
                         .requestMatchers("/api/v1/profile").hasRole("USER")
                         .requestMatchers("/api/v1/update/user/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/delete/**").hasRole("ADMIN")
@@ -36,9 +42,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/users").hasRole("ADMIN")
                         .requestMatchers("/api/v1/user/events/*/register").hasRole("USER")
                         .requestMatchers("/api/v1/user/registrations").hasRole("USER")
-                .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults()); // pour travailler avec method authentification http basic
-        return http.build();
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
 
+        return http.build();
     }
 }
